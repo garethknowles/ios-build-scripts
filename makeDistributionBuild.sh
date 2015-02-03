aa88c0819c3ab324b3434476dddf338e5cf86a06
Gareth Knowles <gareth.knowles@smilemachine.com>
2015-02-03 17:23:55 +0100

    Fixed IPA template and updated docs for makeDistributionBuild

--- makeDistributionBuild.sh
+++ makeDistributionBuild.sh
@@ -2,16 +2,17 @@
 
 # "Gareth's Supreme iOS Distribution Builder
 
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]  || [ -z "$4" ]  ;
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]  || [ -z "$4" ]|| [ -z "$5" ]   ;
 then
    echo "=== Gareth's iOS Distribution Builder ==="
-   echo "Build and Archiver for Distribution Builds. Usage:"
-   echo "makeDistributionBuild projectPath workspaceName schemeName ipaName"
-   echo "The default action is to create an index.html file and a manifest.plist file based on ipa provided"
-   echo "projectPath: the relative path to the root of the project"
-   echo "workspaceName: name of the workspace"
-   echo "schemeName: name of scheme"
-   echo "ipaName: the name ouf the ipaFile"
+   echo "Build and Archiver for Distribution Builds. Usage:\n"
+   echo "./makeDistributionBuild <projectPath> <workspaceName> <schemeName> <signingIdentitiy> <ipaName>\n"
+   echo "The default action is to create an index.html file and a manifest.plist file based on the given inputs:"
+   echo "  projectPath:       the relative path to the root of the project"
+   echo "  workspaceName:     name of the workspace"
+   echo "  schemeName:        name of scheme"
+   echo "  signingIdentity:   the name of the signing identity to use for the build"
+   echo "  ipaName:           the name of the ipaFile"
    exit
 fi
 
@@ -24,7 +25,10 @@ echo "WORKSPACE_NAME=$WORKSPACE_NAME"
 SCHEME_NAME=$3
 echo "SCHEME_NAME=$SCHEME_NAME"
 
-IPA_NAME=$4
+SIGNING_IDENTIITY=$4
+echo "SIGNING_IDENTIITY=$IPA_NAME"
+
+IPA_NAME=$5
 echo "IPA_NAME=$IPA_NAME"
 
 # 1. Clean Old Data
@@ -58,6 +62,6 @@ xctool \
 rm -f $PROJECT_PATH/build/$IPA_NAME.ipa
 xcodebuild -archivePath $PROJECT_PATH/build/$IPA_NAME.xcarchive\
   -exportArchive \
-  -exportSigningIdentity "iPhone Distribution: Smile Machine Limited" \
+  -exportSigningIdentity $SIGNING_IDENTIITY \
   -exportFormat ipa \
   -exportPath $PROJECT_PATH/build/$IPA_NAME.ipa
