1b4696dd824a0ba805275a2aeb768d6c5f920b6c
Gareth Knowles <gareth.knowles@news.co.uk>
2015-08-25 15:22:22 +0100

    Fixes to script

--- enterpiseDistributionWebpage.sh
+++ enterpiseDistributionWebpage.sh
@@ -9,7 +9,7 @@ then
    echo "enterpriseDistributionWebpage ipaPath fullURL outputPath"
    echo "The default action is to create an index.html file and a manifest.plist file based on ipa provided"
    echo "ipaPath: the path to the input ipa file"
-   echo "fullURL: the absolute URL where your deployment of the ipa file will happen"   
+   echo "fullURL: the absolute URL where your deployment of the ipa file will happen"
    echo "outputPath: the path to output the files locally to"
    exit
 fi
@@ -25,7 +25,8 @@ OUTPUT_PATH=$3
 
 rm -Rf $OUTPUT_PATH
 mkdir -p $OUTPUT_PATH
-cp sbin/IPATemplate/*.* $OUTPUT_PATH/.
+cp IPATemplate/index.html $OUTPUT_PATH/.
+cp IPATemplate/manifest.plist $OUTPUT_PATH/.
 cp $IPA_PATH $OUTPUT_PATH/$IPA_FILE
 
 unzip -j $IPA_PATH Payload/*.app/Info.plist -d $OUTPUT_PATH/.
@@ -42,8 +43,16 @@ BUNDLE_NAME=`/usr/libexec/PlistBuddy -c "Print CFBundleName" $OUTPUT_PATH/Info.p
 IPA_SIZE=`ls -nl $IPA_PATH | awk '{print $5}'`
 # grabbing ipa md5
 IPA_MD5=`md5 -q $IPA_PATH`
-                 
-`/usr/libexec/PlistBuddy -c "Set items:0:metadata:bundle-identifier $BUNDLE_IDENTIFIER" $OUTPUT_PATH/manifest.plist`                 
+
+echo "Found Values:"
+echo $BUNDLE_IDENTIFIER
+echo $COMERCIAL_VERSION
+echo $BUILD_VERSION
+echo $BUNDLE_NAME
+echo $IPA_SIZE
+echo $IPA_MD5
+
+`/usr/libexec/PlistBuddy -c "Set items:0:metadata:bundle-identifier $BUNDLE_IDENTIFIER" $OUTPUT_PATH/manifest.plist`
 `/usr/libexec/PlistBuddy -c "Set items:0:metadata:bundle-version $COMERCIAL_VERSION ($BUILD_VERSION)" $OUTPUT_PATH/manifest.plist`
 `/usr/libexec/PlistBuddy -c "Set items:0:metadata:title $BUNDLE_NAME" $OUTPUT_PATH/manifest.plist`
 `/usr/libexec/PlistBuddy -c "Set items:0:assets:0:url $FULL_URL/$IPA_FILE" $OUTPUT_PATH/manifest.plist`
@@ -57,4 +66,4 @@ echo $TIMESTAMP
 export REPLACE_FULL_URL=s!{FULL_URL}!$FULL_URL!g
 export REPLACE_BUNDLE_NAME=s/{BUNDLE_NAME}/$BUNDLE_NAME/g
 export REPLACE_TIMESTAMP=s!{TIMESTAMP}!$TIMESTAMP!g
-sed -e $REPLACE_FULL_URL -e "$REPLACE_BUNDLE_NAME" -e "$REPLACE_TIMESTAMP" sbin/IPATemplate/index.html > $OUTPUT_PATH/index.html
+sed -e $REPLACE_FULL_URL -e "$REPLACE_BUNDLE_NAME" -e "$REPLACE_TIMESTAMP" IPATemplate/index.html > $OUTPUT_PATH/index.html
